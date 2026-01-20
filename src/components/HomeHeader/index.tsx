aa76e0c5e2dff9a3b80229a5b4c9fec22e62ba59
Jorge Roberto Argolo <jorgerobertoargolo@gmail.com>
2026-01-20 18:34:47 -0300

    First commit

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,12 +1,42 @@
 import { LinearGradient } from "expo-linear-gradient";
+import { View, Text } from "react-native";
+
 
 import { styles } from "./styles";
 import { colors } from "@/theme/colors";
+import { Separator } from "../Separator";
+import { Summary, SummaryProps } from "../Summary";
 
-export function HomeHeader () {
+export type HomeHeaderProps = {
+    total: string;
+    input: SummaryProps;
+    output: SummaryProps;
+}
+
+type Props = {
+    data: HomeHeaderProps;
+}
+
+export function HomeHeader ({ data }: Props) {
     return (
         <LinearGradient colors={[colors.blue[500], colors.blue[800]]} style={styles.container}>
+            <View>
+                <Text style={styles.label}>Total que você possui</Text>
+                <Text style={styles.total}>{data.total}</Text>
+            </View>
+            <Separator color={colors.blue[400]} />
+            <View style={styles.summary}>
+                <Summary 
+                    data={data.input} 
+                    icon={{ name: 'arrow-upward', color: colors.green[500] }}    
+                />
 
+                <Summary 
+                    data={data.output} 
+                    icon={{ name: 'arrow-downward', color: colors.red[400] }}
+                    isLeft={true}
+                />
+            </View>
         </LinearGradient>
     );
 }
