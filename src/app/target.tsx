ef000f6821fdc69ae7ab63d6618fe0c82dcc8bd0
Jorge Roberto Argolo <jorgerobertoargolo@gmail.com>
2026-01-26 11:02:17 -0300

    feat: Componente do Input

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,7 +1,8 @@
-import { View, Button } from "react-native";
-import { router } from "expo-router";
+import { View } from "react-native";
 
 import { PageHeader } from "@/components/PageHeader";
+import { Input } from "@/components/Input";
+import { Button } from "@/components/Button";
 
 export default function Target () {
     return (
@@ -14,6 +15,17 @@ export default function Target () {
                     onPress: () => {},
                 }}
             />
+
+            <View style={{ marginTop: 32, gap: 24 }}>
+                <Input 
+                    label="Nome da meta"
+                    placeholder="Ex: Viagem para praia, Apple Watch"    
+                />
+                <Button
+                    title="Salvar"
+                />
+            </View>
+
         </View>
     );
 }
