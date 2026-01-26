cf4a5c2fea73afe66130310b1bb8a75f0759b684
Jorge Roberto Argolo <jorgerobertoargolo@gmail.com>
2026-01-26 11:37:48 -0300

    feat: Componente de progresso

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,13 +1,31 @@
-import { Button, Text, View } from "react-native";
-import { router, useLocalSearchParams } from "expo-router";
+import { View } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+
+import { PageHeader } from "@/components/PageHeader";
+import { Progress } from "@/components/Progress";
+
+const details = {
+    current: 'R$ 580,00',
+    target: 'R$ 1.790,00',
+    percentage: 25
+}
 
 export default function InProgress () {
     const params = useLocalSearchParams<{id: string}>();
 
     return (
-        <View style={{ flex: 1, justifyContent: 'center'}}>
-            <Text>InProgress: {params.id}</Text>
-            <Button title="Voltar" onPress={() => router.back()} />
+        <View style={{ flex: 1, padding: 24, gap: 32}}>
+            <PageHeader
+                title="Apple Watch"
+                rightButton={{
+                    icon: 'edit',
+                    onPress: () => {},
+                }}
+            />
+
+            <Progress 
+                data={details}
+            />
         </View>
     );
 }
