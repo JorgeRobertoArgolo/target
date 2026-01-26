5543a3237df2e92dddb8407d7bc5e0640633a195
Jorge Roberto Argolo <jorgerobertoargolo@gmail.com>
2026-01-26 11:13:46 -0300

    feat: Adicionando Input de valor formatado

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -3,6 +3,7 @@ import { View } from "react-native";
 import { PageHeader } from "@/components/PageHeader";
 import { Input } from "@/components/Input";
 import { Button } from "@/components/Button";
+import { CurrencyInput } from "@/components/CurrencyInput";
 
 export default function Target () {
     return (
@@ -10,10 +11,6 @@ export default function Target () {
             <PageHeader 
                 title="Meta" 
                 subtitle="Economize para alcançar sua meta financeira"
-                rightButton={{
-                    icon: 'edit',
-                    onPress: () => {},
-                }}
             />
 
             <View style={{ marginTop: 32, gap: 24 }}>
@@ -21,6 +18,13 @@ export default function Target () {
                     label="Nome da meta"
                     placeholder="Ex: Viagem para praia, Apple Watch"    
                 />
+
+                <CurrencyInput 
+                    label="Valor alvo"
+                    value={0}
+
+                />
+
                 <Button
                     title="Salvar"
                 />
