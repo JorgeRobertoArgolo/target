cfc6ef0bf7374faab924d97f76e73b35140b328d
Jorge Roberto Argolo <jorgerobertoargolo@gmail.com>
2026-01-26 12:29:30 -0300

    feat: componente de opção na tela de nova transação

--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import { View } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { PageHeader } from "@/components/PageHeader";
 import { CurrencyInput } from "@/components/CurrencyInput";
 import { Input } from "@/components/Input";
 import { Button } from "@/components/Button";
+import { TransactionType } from "@/components/TransactionType";
+import { TransactionTypes } from "@/utils/TransactionTypes";
 
 export default function Transaction () {
-    
+
+    const [type, setType] = useState(TransactionTypes.Input);
     const params = useLocalSearchParams<{id: string}>();
 
     return (
@@ -16,6 +20,12 @@ export default function Transaction () {
                 subtitle="A cada valor guardado você fica mais próximo da sua meta. Se esforce para guardar e evitar retirar."
             />
             <View style={{marginTop: 32, gap: 24}}>
+
+                <TransactionType 
+                    selected={type}
+                    onChange={setType}
+                />
+
                 <CurrencyInput
                     label="Valor (R$)"
                     value={0}
